09e7b400b3b2e07e9b24b8334a0d23f4211d3457
Tusharpankhaniya <pankhaniyatushar9@gmail.com>
2025-01-02 08:50:30 +0530

    solved some problem

---
+++ twoSum.js
@@ -0,0 +1,14 @@
+    const arr = [2, 7]
+    const target = 9
+
+    function twoSum(arr, target) { 
+        for(let i = 0; i < arr.length; i++) {
+            for(let j = i + 1; j < arr.length; j++) {
+                if(arr[i] + arr[j] === target) {
+                    return [i, j]
+                }
+            }
+        }
+    }
+
+    console.log(twoSum(arr, target))
