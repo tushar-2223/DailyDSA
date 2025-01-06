b9be42ee27eae78e7569313bbb3fb4c0c3e51cc4
Tusharpankhaniya <pankhaniyatushar9@gmail.com>
2025-01-06 21:19:39 +0530

    buy and sell stock algorithm

---
+++ buyAndsellStocks.js
@@ -0,0 +1,19 @@
+let prices = [7, 1, 5, 3, 6, 4];
+
+// solution 1
+const maxProfit = (prices) => {
+    let maxProfit = 0;
+    let minPrice = prices[0];
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    return maxProfit;
+}
+
+console.log(maxProfit(prices));
