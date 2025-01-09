dc4f5a6056ac851a942f2839f43c7676f5c35a19
Tusharpankhaniya <pankhaniyatushar9@gmail.com>
2025-01-09 22:25:28 +0530

    merge Two Sorted linked list

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -1,11 +1,32 @@
+// <========= Helper class and functions for singly linked list =========>
+
 class Node {
   constructor(data1, next1 = null) {
       this.data = data1;
       this.next = next1;
   }
 }
+// Function to print the linked list
+function printLinkedList(head) {
+  let temp = head;
+  while (temp !== null) {
+      console.log(temp.data);
+      temp = temp.next;
+  }
+  console.log('null');
+}
+function genrateLinkedListToArr(arr) {
+  let head = new Node(arr[0]);
+  let temp = head;
+  for (let i = 1; i < arr.length; i++) {
+      temp.next = new Node(arr[i]);
+      temp = temp.next;
+  }
+  return head;
+}
 
-// Function to merge two sorted linked lists
+//***************  Merge two sorted linked lists ***************
+
 function sortTwoLinkedLists(list1, list2) {
   const dummyNode = new Node(-1);
   let temp = dummyNode;
@@ -33,38 +54,9 @@ function sortTwoLinkedLists(list1, list2) {
   return dummyNode.next;
 }
 
-// Function to print the linked list
-function printLinkedList(head) {
-  let temp = head;
-  while (temp !== null) {
-      process.stdout.write(temp.data + " ");
-      temp = temp.next;
-  }
-  console.log();
-}
-
-function genrateLinkedListToArr(arr) {
-  let head = new Node(arr[0]);
-  let temp = head;
-  for (let i = 1; i < arr.length; i++) {
-      temp.next = new Node(arr[i]);
-      temp = temp.next;
-  }
-  return head;
-}
-
-let list1 = genrateLinkedListToArr([1, 3, 5, 7, 9]);
-let list2 = genrateLinkedListToArr([2, 4, 6, 8, 10]);
-
-process.stdout.write("First sorted linked list: ");
-printLinkedList(list1);
-
-process.stdout.write("Second sorted linked list: ");
-printLinkedList(list2);
+let list1 = genrateLinkedListToArr([1, 2, 4]);
+let list2 = genrateLinkedListToArr([1, 3, 4]);
 
 let mergedList = sortTwoLinkedLists(list1, list2);
 
-process.stdout.write("Merged sorted linked list: ");
 printLinkedList(mergedList);
-                          
-                      
