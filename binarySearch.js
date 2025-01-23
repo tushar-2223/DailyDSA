5afc3d9b1ad464461f7df9a8f77afd2be33e6c0d
Tusharpankhaniya <pankhaniyatushar9@gmail.com>
2025-01-23 20:54:53 +0530

    Worked on the binary search

--- binarySearch.js
+++ binarySearch.js
@@ -13,3 +13,5 @@ const binarySearch = (nums, low, high, target) => {
 const search = function (nums, target) {
     return binarySearch(nums, 0, nums.length - 1, target);
 };
+
+console.log(search([-1,1,2,4,5,9,10,11,12],9))
